323b4d76a0e95455551771bb3a819aaab2d8fec7
Valentin Fadeev <valentinfs@yandex.ru>
2021-08-14 22:44:17 +0300

    Change Number of Credit Card

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "2204452094480217";
+        String number = "6386496807492468";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
